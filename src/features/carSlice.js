d164fadc1258eecf419ebb63108c7be13b67f80c
irvantaufik28 <irvantaufik28@gmail.com>
2023-05-21 11:54:00 +0700

    merge conflict

---
+++ src/features/carSlice.js
@@ -0,0 +1,38 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+import config from '../config';
+
+
+export const getAllCars = createAsyncThunk("car/getAllCars", async (params = {}) => {
+    const apiUrl = config.apiBaseUrl
+    const response = await axios.get(apiUrl + "/customer/v2/car", {
+        params
+    })
+    return response.data
+})
+
+
+const carSlice = createSlice({
+    name: "car",
+    initialState: {
+        data: {},
+        loading: false,
+        addCarResponse: {},
+    },
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(getAllCars.fulfilled, (state, action) => {
+                state.data = action.payload
+                state.loading = false
+            })
+            .addCase(getAllCars.pending, (state, action) => {
+                state.loading = true
+            })
+    }
+})
+export const carSelectors = {
+    selectAllCars: (state) => state.car.data,
+    loading: (state) => state.car.loading,
+}
+export default carSlice.reducer;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import IndexRoutes from './routes/index.routes';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import store from './store';
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <React.StrictMode>
+    <Provider store={store}>
+     <IndexRoutes />
+    </Provider>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
