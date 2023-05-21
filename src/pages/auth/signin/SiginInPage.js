1e5b928008fdd22656c06afde7bff00503b03339
irvantaufik28 <irvantaufik28@gmail.com>
2023-05-21 16:17:54 +0700

    login customer

--- src/pages/auth/signin/SiginInPage.js
+++ src/pages/auth/signin/SiginInPage.js
@@ -1,15 +1,29 @@
 import React from 'react'
+import jwtDecode from "jwt-decode";
 import SignIn from './components/SignIn';
-
+import { useDispatch } from "react-redux";
+import { useNavigate } from 'react-router-dom';
+import { login } from '../../../features/authSlice';
 
 
 const SiginInPage = () => {
+  const dispacth = useDispatch();
+  const navigate = useNavigate()
+  const submit = async (payload) => {
 
+    try {
+      const result = await dispacth(login(payload)).unwrap();
+      const user = jwtDecode(result.access_token)
+      localStorage.setItem('token', result.access_token)
 
+    } catch (error) {
 
-
-  const submit = (payload) => {
-    console.log(payload)
+      // todo get error message
+       
+      console.log(error.message)      
+      return
+    }
+    navigate('/')
   }
 
   return (
@@ -18,7 +32,5 @@ const SiginInPage = () => {
     </>
   )
 }
-
-
 
 export default SiginInPage;
