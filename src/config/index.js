fa5bcfdb32d95461a3c5bc6e2cbb406e982dc37c
irvantaufik28 <irvantaufik28@gmail.com>
2023-05-21 11:48:57 +0700

    merge masitad

---
+++ src/config/index.js
@@ -0,0 +1,10 @@
+
+const config = {
+    appName: process.env.REACT_APP_NAME,
+    apiBaseUrl: process.env.REACT_APP_API_BASE_URL,
+    apiBasePath: process.env.REACT_APP_API_BASE_PATH,
+    apiUrl:
+      process.env.REACT_APP_API_BASE_URL + process.env.REACT_APP_API_BASE_PATH,
+  }
+  
+  export default config
