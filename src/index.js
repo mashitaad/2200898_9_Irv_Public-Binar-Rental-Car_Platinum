a230b9550c4a57df0baab7aaaf19d78627081dd7
irvantaufik28 <irvantaufik28@gmail.com>
2023-05-21 12:01:02 +0700

    merger

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
-<<<<<<< HEAD
 import IndexRoutes from './routes/index.routes';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import store from './store';
@@ -14,15 +13,6 @@ root.render(
      <IndexRoutes />
     </Provider>
   </React.StrictMode>
-=======
-import { BrowserRouter } from 'react-router-dom';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
->>>>>>> 1715d51 (refactor: Modified Browser Router)
 );
 
 // If you want to start measuring performance in your app, pass a function
