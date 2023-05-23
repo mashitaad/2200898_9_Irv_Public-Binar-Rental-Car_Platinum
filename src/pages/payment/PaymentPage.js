b9f102fa02615980b835b65b6027837c80419a2d
irvantaufik28 <irvantaufik28@gmail.com>
2023-05-23 20:54:52 +0700

    component order detail

--- src/pages/payment/PaymentPage.js
+++ src/pages/payment/PaymentPage.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import OrderDetail from './component/OrderDetail'
 
 export const PaymentPage = () => {
+
+    const orderDetailData = localStorage.getItem('order_detail')
+    console.log(JSON.parse(orderDetailData))
   return (
-    <div>PaymentPage</div>
+    <>
+    <OrderDetail data={JSON.parse(orderDetailData)} />
+    </>
   )
 }
