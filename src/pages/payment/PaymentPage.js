3e70d68f0c5e6eedd2ee18554215d062d0ddcc77
irvantaufik28 <irvantaufik28@gmail.com>
2023-05-23 21:36:30 +0700

    payment component

--- src/pages/payment/PaymentPage.js
+++ src/pages/payment/PaymentPage.js
@@ -1,13 +1,24 @@
 import React from 'react'
 import OrderDetail from './component/OrderDetail'
+import Payment from './component/Payment'
 
 export const PaymentPage = () => {
 
     const orderDetailData = localStorage.getItem('order_detail')
-    console.log(JSON.parse(orderDetailData))
+
+    const bankType = (payload) => {
+        console.log(payload)
+    }
   return (
     <>
     <OrderDetail data={JSON.parse(orderDetailData)} />
+    <Payment 
+    data={JSON.parse(orderDetailData)}
+    handleClick={bankType}
+    >
+
+        
+    </Payment>
     </>
   )
 }
