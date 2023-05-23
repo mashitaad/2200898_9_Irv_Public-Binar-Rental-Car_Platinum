f22cb60e34982e77674b5d17740594615e264deb
irvantaufik28 <irvantaufik28@gmail.com>
2023-05-23 20:42:49 +0700

    set localstroge order_detail

--- src/pages/car/carDetail/CarDetailPage.js
+++ src/pages/car/carDetail/CarDetailPage.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
 import CarDetail from './components/CarDetail'
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import FromFilterDetail from '../../../components/from-filter/FromFilterDetail';
 import { useDispatch, useSelector } from 'react-redux';
 import { carSelectors, getCarById } from '../../../features/carSlice';
@@ -8,8 +8,10 @@ import LoadingSpinner from '../../../components/ui/LoadingSpinner';
 import Banner from '../../home/components/Banner';
 import NavbarLayout from '../../../components/layouts/Navbar';
 import FormCalendar from './components/FormCalendar';
+import { format } from 'date-fns';
 
 const CarDetailPage = () => {
+  const navigate = useNavigate()
   const { id } = useParams();
 
   const dispatch = useDispatch();
@@ -23,7 +25,17 @@ const CarDetailPage = () => {
 
 
   const calendarHandle = (payload) => {
-    console.log(payload)
+    const formattedDate = (date) => {
+      return format(date, "yyyy-MM-dd");
+    };
+    let requestOrder = {
+      start_date_at: formattedDate(payload[0]),
+      finish_date_at: formattedDate(payload[1]),
+      car_id: car?.id
+    };
+    const newData = {...requestOrder,...car}
+    localStorage.setItem('order_detail',JSON.stringify(newData))
+    navigate('/payment')
   }
   
 
