b9ac4254ac17142ece9dbff8feec34259eead1ae
hrndbrs <hrndbrs@gmail.com>
2023-05-25 00:12:15 +0700

    registration page api integration

--- src/pages/auth/signup/SignUpPage.js
+++ src/pages/auth/signup/SignUpPage.js
@@ -1,19 +1,35 @@
 import React from 'react'
 import SignUp from './components/SignUp';
+import axios from 'axios';
+import { useNavigate } from 'react-router';
 
 const SignUpPage = () => {
 
+  const navigate = useNavigate()
+
+  const registerCustomer = async (payload) => {
+    try {
+      const url = 'https://bootcamp-rent-cars.herokuapp.com/customer/auth/register';
+
+      const response = await axios.post(url, payload);
+      console.log(response.data); // You can handle the response data here
+      navigate('/signin')
+      // Additional logic after successful registration
+    } catch (error) {
+      console.log(error.response.data);
+      // Handle error appropriately
+    }
+  };
+
+  // registerCustomer();
 
   const handleSignUp = (payload) => {
-    console.log(payload)
   // todo fecth api auth
   // /customer/auth/register
 
-  
-  
+    registerCustomer(payload);
+    console.log(payload)
 
-
-  // if success page login
   }
 
 
