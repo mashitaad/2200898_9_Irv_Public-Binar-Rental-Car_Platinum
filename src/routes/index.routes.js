0d4de5259ab89ed5e8d8c578acd8e4e4d07584d6
irvantaufik28 <irvantaufik28@gmail.com>
2023-05-25 09:34:29 +0700

    pull dev

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -18,7 +18,7 @@ function IndexRoutes() {
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/car/list" element={<CarListPage />} />
         <Route path="/car/list/:id" element={<CarDetailPage />} />
-     
+       
         <Route element={<PrivateRoutesCustomer />}>
         <Route path="/payment" element={<PaymentPage />} />
         <Route path="/payment/confirm/order/:id" element={<PaymentConfirmationPage />} />
