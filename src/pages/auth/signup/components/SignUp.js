94bb7e3f21c4778841433d22e813f79cffe89983
unknown <sadynursholihin@gmail.com>
2023-05-28 19:17:03 +0700

    Fixed Validation Message SignUp

--- src/pages/auth/signup/components/SignUp.js
+++ src/pages/auth/signup/components/SignUp.js
@@ -1,136 +1,139 @@
-import React, { useState } from 'react'
-import logo from '../../../../assets/images/rectangle.png';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import { NavLink } from 'react-router-dom';
-// import { useNavigate } from 'react-router-dom'
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { NavLink } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import logo from "../../../../assets/images/rectangle.png";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import { register } from "../../../../features/registerSlice";
 
 const SignUp = (props) => {
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+    role: "customer",
+  });
 
-    // const history = useNavigate();
+  const dispatch = useDispatch();
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
-    const [form, setForm] = useState({
-        email: "",
-        password: "",
-        role: "customer"
-    })
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+    setIsSubmitting(true);
 
-    // const [data, setData] = useState([]);
-    // console.log(inpval);
+    // Validasi password
+    if (form.password.length < 6) {
+      setIsSubmitting(false);
+      toast.error("Password must be at least 6 characters long", {
+        position: "top-center",
+      });
+      return;
+    }
 
-    // const getdata = (e) => {
-        // console.log(e.target.value);
-        // const { value, name } = e.target;
-        // console.log(value,name);
+    try {
+      const response = await dispatch(register(form));
+      setIsSubmitting(false);
 
-    //     setInpval(() => {
-    //         return {
-    //             ...inpval,
-    //             [name]: value
-    //         }
-    //     })
-    // }
+      if (response.error) {
+        toast.error(response.payload.message, {
+          position: "top-center",
+        });
+      }
+    } catch (error) {
+      setIsSubmitting(false);
+      toast.error("An error occurred during registration", {
+        position: "top-center",
+      });
+    }
+  };
 
-    // const addData = (e) => {
-    //     e.preventDefault();
+  return (
+    <>
+      <div className="container">
+        <section className="d-flex justify-content-between">
+          <div
+            className="left_data"
+            style={{
+              width: "45%",
+              display: "flex",
+              justifyContent: "center",
+              flexDirection: "column",
+              paddingLeft: "16vh",
+            }}
+          >
+            <img
+              src={logo}
+              style={{ width: "15%" }}
+              className="signup-logo"
+              alt="logo"
+            />
+            <h3 className="text-start mt-4 col-lg-6">Sign Up</h3>
 
-    //     // console.log(inpval);
-    //     const {name,email,password} = inpval;
+            <Form onSubmit={handleSignUp}>
+              <Form.Group className="mb-3 col-lg-9" controlId="formBasicEmail">
+                <Form.Label>Email*</Form.Label>
+                <Form.Control
+                  type="email"
+                  name="email"
+                  value={form.email}
+                  onChange={(e) =>
+                    setForm({
+                      ...form,
+                      email: e.target.value,
+                    })
+                  }
+                  placeholder="Contoh: johndee@gmail.com"
+                />
+              </Form.Group>
 
-    //     if (name === "") {
-    //         alert("name field is requred")
-    //     } else if(email === "") {
-    //         alert("email field is requred")
-    //     } else if(!email.includes("@")) {
-    //         alert("please enter valid email address")
-    //     } else if(password === "") {
-    //         alert("password field is requred")
-    //     } else if(password.length < 6) {
-    //         alert("password length must be greater than six character")
-    //     } else {
-    //         console.log("data added succesfully");
-    //         localStorage.setItem("userinput",JSON.stringify([...data,inpval]));
-    //     }
-    //         toast.error('name field is requred!',{
-    //             position: "top-center",
-    //         });
-    //     } else if (email === "") {
-    //          toast.error('email field is requred',{
-    //             position: "top-center",
-    //         });
-    //     } else if (!email.includes("@")) {
-    //          toast.error('plz enter valid email addres',{
-    //             position: "top-center",
-    //         });
-    //     } else if (password === "") {
-    //          toast.error('password field is requred',{
-    //             position: "top-center",
-    //         });
-    //     } else if (password.length < 6) {
-    //          toast.error('password length greater six',{
-    //             position: "top-center",
-    //         });
-    //     } else {
-    //         console.log("data added succesfully");
-    //         history("/login")
-    //         localStorage.setItem("useryoutube",JSON.stringify([...data,inpval]));
+              <Form.Group
+                className="mb-3 col-lg-9"
+                controlId="formBasicPassword"
+              >
+                <Form.Label>Create Password*</Form.Label>
+                <Form.Control
+                  type="password"
+                  name="password"
+                  value={form.password}
+                  onChange={(e) =>
+                    setForm({
+                      ...form,
+                      password: e.target.value,
+                    })
+                  }
+                  placeholder="6+ karakter"
+                />
+              </Form.Group>
 
-    // }
-
-    // }
-
-    return (
-        <>
-            <div className='container'>
-                <section className='d-flex justify-content-between'>
-                    <div className="left_data" style={{ width: "45%", display: "flex", justifyContent: "center", flexDirection: "column", paddingLeft: "16vh" }}>
-                        <img src={logo} style={{ width: "15%" }} className="signup-logo" alt="logo" />
-                        <h3 className='text-start mt-4 col-lg-6'>Sign Up</h3>
-
-                        <Form onSubmit={(e => {
-                            e.preventDefault()
-                            props.onSubmit(form)
-                        })}>
-                          
-
-                            <Form.Group className="mb-3 col-lg-9" controlId="formBasicEmail">
-                                <Form.Label>Email*</Form.Label>
-                                <Form.Control type="email" name='email' onChange={e => setForm({
-                                    ...form, ...{ email: e.target.value }
-                                })} placeholder="Contoh: johndee@gmail.com" />
-                            </Form.Group>
-
-                            <Form.Group className="mb-3 col-lg-9" controlId="formBasicPassword">
-                                <Form.Label>Create Password*</Form.Label>
-                                <Form.Control type="password" name='password' onChange={e => setForm({
-                                    ...form, ...{ password: e.target.value }
-                                })} placeholder="6+ karakter" />
-                            </Form.Group>
-
-                            <Button variant="primary" className='col-lg-9 mt-3' nChange={e => setForm({
-                                    ...form, ...{ password: e.target.value }
-                                })} style={{ background: "rgb(13,40,166)" }} type="submit">
-                                SignUp
-                            </Button>
-                        </Form>
-                        <p className='mt-3'>Already have an account? <span><NavLink to="/signin">Sign In here</NavLink></span></p>
-
-                    </div>
-                    <div className="right_data" style={{ width: "45%" }}>
-                        <img src="./sign_img.png" style={{ width: "100%", height: "100%", objectFit: "cover" }} alt="" />
-                    </div>
-                </section>
-                <ToastContainer />
-            </div>
-        </>
-    )
-}
+              <Button
+                variant="primary"
+                className="col-lg-9 mt-3"
+                style={{ background: "rgb(13,40,166)" }}
+                type="submit"
+              >
+                {isSubmitting ? "Loading..." : "SignUp"}
+              </Button>
+            </Form>
+            <p className="mt-3">
+              Already have an account?{" "}
+              <span>
+                <NavLink to="/signin">Sign In here</NavLink>
+              </span>
+            </p>
+          </div>
+          <div className="right_data" style={{ width: "45%" }}>
+            <img
+              src="./sign_img.png"
+              style={{ width: "100%", height: "100%", objectFit: "cover" }}
+              alt=""
+            />
+          </div>
+        </section>
+        <ToastContainer position="top-center" />
+      </div>
+    </>
+  );
+};
 
 export default SignUp;
-
-
-SignUp.defaultProps = {
-    onSubmit: () => {}
-  }
