01da8d223efd8254f803b0af0cf6f16804658e58
unknown <sadynursholihin@gmail.com>
2023-05-28 20:05:15 +0700

    fixed validate mesagge

--- src/pages/auth/signup/components/SignUp.js
+++ src/pages/auth/signup/components/SignUp.js
@@ -1,8 +1,12 @@
 import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { NavLink } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import logo from "../../../../assets/images/rectangle.png";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
-import { NavLink } from "react-router-dom";
+import { register } from "../../../../features/registerSlice";
 
 const SignUp = (props) => {
   const [form, setForm] = useState({
@@ -11,43 +15,47 @@ const SignUp = (props) => {
     role: "customer",
   });
 
-  const [errorMessage, setErrorMessage] = useState({});
+  const dispatch = useDispatch();
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setForm({ ...form, [name]: value });
-  };
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+    setIsSubmitting(true);
 
-  const submitClick = () => {
-    setErrorMessage(validate(form));
-  };
-
-  const validate = (values) => {
-    const errors = {};
-    const regex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/i;
-
-    if (!values.email) {
-      errors.email = " Email is required";
-    } else if (!regex.test(values.email)) {
-      errors.email = " Enter a valid email address";
+    // Validasi password
+    if (form.password.length < 6) {
+      setIsSubmitting(false);
+      toast.error("Password must be at least 6 characters long", {
+        position: "top-center",
+      });
+      return;
     }
 
-    if (!values.password) {
-      errors.password = " Password is required";
-    } else if (values.password.length < 6) {
-      errors.password = " Password should contain 6 characters or more";
+    try {
+      const response = await dispatch(register(form));
+      setIsSubmitting(false);
+
+      if (response.error) {
+        toast.error(response.payload.message, {
+          position: "top-center",
+        });
+      }
+    } catch (error) {
+      setIsSubmitting(false);
+      toast.error("An error occurred during registration", {
+        position: "top-center",
+      });
     }
-    return errors;
   };
 
   return (
     <>
       <div className="container">
-        <section className="d-flex">
+        <section className="d-flex justify-content-between">
           <div
             className="left_data"
             style={{
-              flex: "0 0 50%",
+              width: "45%",
               display: "flex",
               justifyContent: "center",
               flexDirection: "column",
@@ -62,21 +70,21 @@ const SignUp = (props) => {
             />
             <h3 className="text-start mt-4 col-lg-6">Sign Up</h3>
 
-            <Form
-              onSubmit={(e) => {
-                e.preventDefault();
-                props.onSubmit(form);
-              }}
-            >
+            <Form onSubmit={handleSignUp}>
               <Form.Group className="mb-3 col-lg-9" controlId="formBasicEmail">
                 <Form.Label>Email*</Form.Label>
                 <Form.Control
                   type="email"
                   name="email"
-                  onChange={handleChange}
+                  value={form.email}
+                  onChange={(e) =>
+                    setForm({
+                      ...form,
+                      email: e.target.value,
+                    })
+                  }
                   placeholder="Contoh: johndee@gmail.com"
                 />
-                {errorMessage.email ?? <span>{errorMessage.email}</span>}
               </Form.Group>
 
               <Form.Group
@@ -87,74 +95,45 @@ const SignUp = (props) => {
                 <Form.Control
                   type="password"
                   name="password"
-                  onChange={handleChange}
+                  value={form.password}
+                  onChange={(e) =>
+                    setForm({
+                      ...form,
+                      password: e.target.value,
+                    })
+                  }
                   placeholder="6+ karakter"
                 />
-                {errorMessage.password ?? <span>{errorMessage.password}</span>}
               </Form.Group>
 
               <Button
                 variant="primary"
                 className="col-lg-9 mt-3"
-                onClick={submitClick}
                 style={{ background: "rgb(13,40,166)" }}
                 type="submit"
               >
-                SignUp
+                {isSubmitting ? "Loading..." : "SignUp"}
               </Button>
             </Form>
             <p className="mt-3">
-              Already have an account?
+              Already have an account?{" "}
               <span>
-                <NavLink to="/signin"> Sign In here</NavLink>
+                <NavLink to="/signin">Sign In here</NavLink>
               </span>
             </p>
           </div>
-          <div
-            className="right_data"
-            style={{
-              flex: "0 0 100%",
-              backgroundColor: "#0D28A6",
-              height: "100vh",
-              position: "relative",
-            }}
-          >
-            <p
-              style={{
-                position: "absolute",
-                width: "429px",
-                height: "72px",
-                left: "115px",
-                top: "90px",
-                fontFamily: "Rubik",
-                fontStyle: "normal",
-                fontWeight: 500,
-                fontSize: "48px",
-                lineHeight: "150%",
-                color: "#D9D9D9",
-              }}
-            >
-              Binar rental car
-            </p>
+          <div className="right_data" style={{ width: "45%" }}>
             <img
               src="./sign_img.png"
+              style={{ width: "100%", height: "100%", objectFit: "cover" }}
               alt=""
-              style={{
-                position: "absolute",
-                left: "115px",
-                top: "200px",
-                maxHeight: "calc(100vh - 200px)",
-              }}
             />
           </div>
         </section>
+        <ToastContainer position="top-center" />
       </div>
     </>
   );
 };
 
 export default SignUp;
-
-SignUp.defaultProps = {
-  onSubmit: () => {},
-};
