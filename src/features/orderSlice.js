1cf85de75cb87e94ac1ab10a530324a569dfa51f
irvantaufik28 <irvantaufik28@gmail.com>
2023-06-15 15:16:23 +0700

    fix payment upload

--- src/features/orderSlice.js
+++ src/features/orderSlice.js
@@ -7,13 +7,13 @@ import axios from "axios";
 
 export const customerGetOrderById = createAsyncThunk("customer/order/id", async (id) => {
 
-    const token= document.cookie
+    const token = document.cookie
         .split('; ')
         .find((row) => row.startsWith('token='))
         ?.split('=')[1];
 
     const apiUrl = config.apiBaseUrl
-    const response = await axios.get(apiUrl + `/customer/order/${id}`,{
+    const response = await axios.get(apiUrl + `/customer/order/${id}`, {
         headers: {
             access_token: token
         }
@@ -24,7 +24,7 @@ export const customerGetOrderById = createAsyncThunk("customer/order/id", async 
 
 
 export const customerGetAllOrder = createAsyncThunk('order/customer/getAllOrder', async (params = {}) => {
-    const token= document.cookie
+    const token = document.cookie
         .split('; ')
         .find((row) => row.startsWith('token='))
         ?.split('=')[1];
@@ -47,7 +47,7 @@ export const customerGetAllOrder = createAsyncThunk('order/customer/getAllOrder'
 
 
 export const customerDeletOrderById = createAsyncThunk('order/customer/delete', async (id) => {
-    const token= document.cookie
+    const token = document.cookie
         .split('; ')
         .find((row) => row.startsWith('token='))
         ?.split('=')[1];
@@ -55,8 +55,8 @@ export const customerDeletOrderById = createAsyncThunk('order/customer/delete', 
     const apiUrl = config.apiBaseUrl;
 
     try {
-       
-       await axios.delete(apiUrl + `/customer/order/${id}`, {
+
+        await axios.delete(apiUrl + `/customer/order/${id}`, {
             headers: {
                 access_token: token
             }
@@ -69,10 +69,40 @@ export const customerDeletOrderById = createAsyncThunk('order/customer/delete', 
 });
 
 
+export const customerUpdateOrder = createAsyncThunk("order/customer/update", async ( {id, formData}) => {
+    const token = document.cookie
+        .split('; ')
+        .find((row) => row.startsWith('token='))
+        ?.split('=')[1];
+    const apiUrl = config.apiBaseUrl
+    console.log(id)
+    try {
+        const response = await axios.put(apiUrl + `/customer/order/${id}/slip`,
+           formData, {
+            headers: {
+                "content-type": "multipart/json",
+                access_token: token
+            }
+        })
+
+        return response.data
+    } catch (err) {
+        console.log(err)
+        throw err
+        
+    }
+})
+
+
+
+
 const orderSlice = createSlice({
     name: 'order',
     initialState: {
         data: {},
+        loading: false,
+        errorMessage: null,
+        successUpload : false
     },
     reducers: {
         setOrder: (state, action) => {
@@ -87,11 +117,27 @@ const orderSlice = createSlice({
             .addCase(customerGetAllOrder.fulfilled, (state, action) => {
                 state.data = action.payload
             })
+            .addCase(customerUpdateOrder.fulfilled, (state, action) => {
+                state.data = action.payload
+                state.loading = false
+                state.successUpload = true
+            })
+            .addCase(customerUpdateOrder.pending, (state, action) => {
+                state.loading = true
+            })
+            .addCase(customerUpdateOrder.rejected, (state, action) => {
+                state.errorMessage = 'error'
+                state.loading = false
+            })
+
     }
 })
 export const orderSelector = {
     selectCustomerOrdeyById: (state) => state.order.data,
     selectCustomerAllOrders: (state) => state.order.data,
+    loading: (state) => state.order.loading,
+    errorMessage: (state) => state.order.errorMessage,
+    successUpload: (state) => state.order.successUpload
 
 }
 
--- src/pages/payment/PaymentConfirmPage.js
+++ src/pages/payment/PaymentConfirmPage.js
@@ -1,47 +1,40 @@
 import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { useCookies } from 'react-cookie';
-import { customerGetOrderById, orderSelector } from "../../features/orderSlice";
+import { customerGetOrderById, customerUpdateOrder, orderSelector } from "../../features/orderSlice";
 import ReminderPaymnet from "./component/ReminderPayment";
 import { PaymentConfirm } from "./component/PaymentConfirm";
 import HeaderPayment from "./component/HeaderPayment";
 import NavbarLayout from "../../components/layouts/Navbar";
 import FooterLayout from "../../components/layouts/Footer";
-import config from "../../config";
-import axios from "axios";
 
 export default function PaymentConfirmPage() {
   const { id } = useParams();
   const dispatch = useDispatch();
   const order = useSelector(orderSelector.selectCustomerOrdeyById);
-  const [namaBank, setNamaBank] = useState("");
   const navigate = useNavigate();
-
+  
+  const [namaBank, setNamaBank] = useState("");
+  const [errorMessage, setErrorMessage] = useState('')
+  
   useEffect(() => {
     dispatch(customerGetOrderById(id));
   }, [dispatch, id]);
 
-  const [cookies] = useCookies(['token']);
-  const token = cookies.token;
-  const apiUrl = config.apiBaseUrl;
-  
   const confirmPayment = async (payload) => {
+    console.log(payload)
     const formData = new FormData();
-    formData.append("slip", payload);  
+    formData.append("slip", payload);
     try {
-      
-      await axios.put(`${apiUrl}/customer/order/${id}/slip`, formData, {
-       headers: {
-         access_token: token,
-         "Content-Type": "multipart/form-data",
-       }
-      })
+      await dispatch(customerUpdateOrder({ id, formData })).unwrap()
+
     } catch (error) {
       console.log(error)
+      setErrorMessage('masukan file terlibih dulu')
+
     }
   };
-      
+
   const getOrderData = localStorage.getItem("order_detail");
   const getOrderDataJson = JSON.parse(getOrderData);
   const bankType = getOrderDataJson.bankType;
@@ -70,7 +63,7 @@ export default function PaymentConfirmPage() {
         padingBottom={false}
       />
 
-      <PaymentConfirm data={order} handleClick={confirmPayment}>
+      <PaymentConfirm data={order} handleClick={confirmPayment} errorMessage={errorMessage}>
         <ReminderPaymnet data={order} />
       </PaymentConfirm>
       <FooterLayout />
