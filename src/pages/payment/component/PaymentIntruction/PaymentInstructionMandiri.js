0492a0bb78e7b3863d3ee1e9e40d886634b826a1
ramadhan8g <ramadhanyandra13@gmail.com>
2023-07-03 21:11:42 +0700

    payment Instruction

--- src/pages/payment/component/PaymentIntruction/PaymentInstructionMandiri.js
+++ src/pages/payment/component/PaymentIntruction/PaymentInstructionMandiri.js
@@ -23,6 +23,28 @@ const PaymentInstructionMandiri = () => {
                     </ul>
                 </div>
             </Tab>
+            <Tab eventKey="atmbca" title="ATM BCA" >
+                <div className='payment-detail '>
+                    <ul>
+                        <li>Masukkan PIN ATM kamu.</li>
+                        <li>Pada menu utama, pilih menu “Bayar/Beli” lalu pilih menu “Multi Payment” (Jika di layar belum tersedia, tekan menu “Lainnya” dan pilih “Multi Payment“).</li>
+                        <li>Masukkan nomor 88871 pada kode perusahaan kemudian tekan tombol “Benar“.</li>
+                        <li>Masukkan kode pembayaran (kode pembayaran Mandiri billpayment kamu)</li>
+                        <li>Periksa kembali data transaksimu dan selesaikan proses pembayaran.</li>
+                    </ul>
+                </div>
+            </Tab>
+            <Tab eventKey="atmbni" title="ATM BNI" >
+                <div className='payment-detail '>
+                    <ul>
+                        <li>Masukkan PIN ATM kamu.</li>
+                        <li>Pada menu utama, pilih menu “Bayar/Beli” lalu pilih menu “Multi Payment” (Jika di layar belum tersedia, tekan menu “Lainnya” dan pilih “Multi Payment“).</li>
+                        <li>Masukkan nomor 88871 pada kode perusahaan kemudian tekan tombol “Benar“.</li>
+                        <li>Masukkan kode pembayaran (kode pembayaran Mandiri billpayment kamu)</li>
+                        <li>Periksa kembali data transaksimu dan selesaikan proses pembayaran.</li>
+                    </ul>
+                </div>
+            </Tab>
         </Tabs>
     </Nav>
 </div>
